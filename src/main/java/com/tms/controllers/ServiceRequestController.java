180040ce946acf652b45039b912c9080f720ce58
Eyasu Weld <Eyasuweld@gmail.com>
2020-09-22 22:13:15 -0500

    Services controller

--- src/main/java/com/tms/controllers/ServiceRequestController.java
+++ src/main/java/com/tms/controllers/ServiceRequestController.java
@@ -12,7 +12,7 @@ import com.tms.models.FormReceiver;
 import com.tms.service.SimpleServiceManager;
 
 @RestController
-@RequestMapping("/email")
+@RequestMapping("/service")
 @CrossOrigin
 public class ServiceRequestController {
 
@@ -24,8 +24,44 @@ public class ServiceRequestController {
 	}
 	
 	@PostMapping("/getAuthority")
-	public ResponseEntity<Object> sendMessage(@RequestBody FormReceiver receiver) {
+	public ResponseEntity<Object> authority(@RequestBody FormReceiver receiver) {
 		service.authorityService(receiver);
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
+	
+	@PostMapping("/cabcard")
+	public ResponseEntity<Object> cabCard(@RequestBody FormReceiver receiver) {
+		service.cabCardService(receiver);
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
+	
+	@PostMapping("/carrierPacket")
+	public ResponseEntity<Object> carrierPacket(@RequestBody FormReceiver receiver) {
+		service.carrierPacketService(receiver);
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
+	
+	@PostMapping("/dispatch")
+	public ResponseEntity<Object> dispatch(@RequestBody FormReceiver receiver) {
+		service.dispatchService(receiver);
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
+	
+	@PostMapping("/form2290")
+	public ResponseEntity<Object> form2290(@RequestBody FormReceiver receiver) {
+		service.form2290Service(receiver);
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
+	
+	@PostMapping("/ifta")
+	public ResponseEntity<Object> ifta(@RequestBody FormReceiver receiver) {
+		service.iftaService(receiver);
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
+	
+	@PostMapping("/ucrRegistration")
+	public ResponseEntity<Object> ucrRegistration(@RequestBody FormReceiver receiver) {
+		service.ucrRegistrationService(receiver);
 		return new ResponseEntity<Object>(HttpStatus.OK);
 	}
 }
--- src/main/java/com/tms/service/SimpleServiceManager.java
+++ src/main/java/com/tms/service/SimpleServiceManager.java
@@ -145,5 +145,4 @@ public class SimpleServiceManager {
 			System.err.println(e.getMessage());
 		}
 	}
-	
 }
