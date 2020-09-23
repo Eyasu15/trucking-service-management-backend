8c611c5271a72f7fce20fd9291ffe7180f3256fb
Eyasu Weld <Eyasuweld@gmail.com>
2020-09-22 22:31:37 -0500

    Bol

--- src/main/java/com/tms/models/Bol.java
+++ src/main/java/com/tms/models/Bol.java
@@ -1,9 +1,15 @@
 package com.tms.models;
 
+import lombok.Data;
+
+@Data
 public class Bol {
 
 	private String loadNumber;
 	private String from;
 	private String to;
+	private Long amount;
+	private String notes;
+	
 	
 }
